f6da20a221427d27ec7ea0a8b5407b5a545e643c
Mallikarjun S K <52494316+mallikarjunsk01@users.noreply.github.com>
2025-12-08 14:15:22 +0530

    Update test_example.py

--- your_automation_files/test_example.py
+++ your_automation_files/test_example.py
@@ -5,7 +5,7 @@ Feature: Login
     When I enter valid credentials
     Then I should see the home page
 
-  #Scenario: Invalid login
+  Scenario: Invalid login
     #Given I am on the login page
     #When I enter invalid credentials
     #Then I should see an error message
